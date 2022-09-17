e5cd4b773a9a97e582d8a8840530cd05655c33cb
Siarhei Paulavets <s.paulavets@gmail.com>
2022-09-17 17:59:18 +0300

    Add xml serializer

---
+++ Tracer.Serialization/Tracer.Serialization.Xml/XmlTraceResultSerializer.cs
@@ -0,0 +1,15 @@
+﻿using Tracer.Core;
+using Tracer.Serialization.Abstractions;
+using XSerializer;
+
+namespace Tracer.Serialization.Xml;
+
+public class XmlTraceResultSerializer: ITraceResultSerializer
+{
+    public string Format => "xml";
+    public void Serialize(TraceResult traceResult, Stream to)
+    {
+        XmlSerializer<TraceResult> serializer = new(new XmlSerializationOptions().Indent());
+        serializer.Serialize(to, traceResult);
+    }
+}
