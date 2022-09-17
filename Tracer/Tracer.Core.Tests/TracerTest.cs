3cc3a3fb688ce138d60934a75bcb52e2d233d14b
Siarhei Paulavets <s.paulavets@gmail.com>
2022-09-17 17:59:22 +0300

    Simplify tests by removing unnecessary asserts

--- Tracer/Tracer.Core.Tests/TracerTest.cs
+++ Tracer/Tracer.Core.Tests/TracerTest.cs
@@ -12,8 +12,6 @@ public class TracerTest
         TraceResult result = tracer.GetTraceResult();
 
         // Assert
-        Assert.NotNull(result);
-        Assert.NotNull(result.Threads);
         Assert.Equal(0, result.Threads.Count);
     }
 
@@ -29,16 +27,9 @@ public class TracerTest
         TraceResult result = tracer.GetTraceResult();
 
         // Assert
-        Assert.NotNull(result);
-
-        Assert.NotNull(result.Threads);
         Assert.Equal(1, result.Threads.Count);
-
-        Assert.NotNull(result.Threads[0].Methods);
         Assert.Equal(1, result.Threads[0].Methods.Count);
-
         AssertM0(result.Threads[0].Methods[0]);
-
         AssertTime(result.Threads[0]);
     }
 
@@ -56,18 +47,11 @@ public class TracerTest
         TraceResult result = tracer.GetTraceResult();
 
         // Assert
-        Assert.NotNull(result);
-
-        Assert.NotNull(result.Threads);
         Assert.Equal(1, result.Threads.Count);
-
-        Assert.NotNull(result.Threads[0].Methods);
         Assert.Equal(3, result.Threads[0].Methods.Count);
-
         AssertM1(result.Threads[0].Methods[0]);
         AssertM0(result.Threads[0].Methods[1]);
         AssertM1(result.Threads[0].Methods[2]);
-
         AssertTime(result.Threads[0]);
     }
 
@@ -90,31 +74,24 @@ public class TracerTest
         TraceResult result = tracer.GetTraceResult();
 
         // Assert
-        Assert.NotNull(result);
-
-        Assert.NotNull(result.Threads);
         Assert.Equal(4, result.Threads.Count);
 
         // First thread
-        Assert.NotNull(result.Threads[0]);
         Assert.Equal(1, result.Threads[0].Methods.Count);
         AssertM0(result.Threads[0].Methods[0]);
         AssertTime(result.Threads[0]);
 
         // Second thread
-        Assert.NotNull(result.Threads[1]);
         Assert.Equal(1, result.Threads[1].Methods.Count);
         AssertM1(result.Threads[1].Methods[0]);
         AssertTime(result.Threads[1]);
 
         // Third thread
-        Assert.NotNull(result.Threads[2]);
         Assert.Equal(1, result.Threads[2].Methods.Count);
         AssertM2(result.Threads[2].Methods[0]);
         AssertTime(result.Threads[2]);
         
         // Fourth thread
-        Assert.NotNull(result.Threads[3]);
         Assert.Equal(1, result.Threads[3].Methods.Count);
         AssertM0(result.Threads[3].Methods[0]);
         AssertTime(result.Threads[3]);
@@ -124,8 +101,6 @@ public class TracerTest
 
     private void AssertM0(MethodInfo method)
     {
-        Assert.NotNull(method);
-
         // Get actual and estimated time in int format.
         bool isParsable = TimeToInt(method, out int actualTime);
         int estimatedTime = WorkloadImitator.GetEstimatedTime(nameof(WorkloadImitator.M0)) + 100;
@@ -139,8 +114,6 @@ public class TracerTest
 
     private void AssertM1(MethodInfo method)
     {
-        Assert.NotNull(method);
-
         // Get actual and estimated time in int format.
         bool isParsable = TimeToInt(method, out int actualTime);
         int estimatedTime = WorkloadImitator.GetEstimatedTime(nameof(WorkloadImitator.M1)) + 100;
@@ -149,15 +122,13 @@ public class TracerTest
         Assert.InRange(actualTime, actualTime, estimatedTime);
         Assert.Equal(nameof(WorkloadImitator.M1), method.Name);
         Assert.Equal(nameof(WorkloadImitator), method.Class);
-        Assert.Equal(2, method.Methods.Count);
+        Assert.Equal(2, method.Methods!.Count);
         AssertM0(method.Methods[0]);
         AssertM0(method.Methods[1]);
     }
 
     private void AssertM2(MethodInfo method)
     {
-        Assert.NotNull(method);
-
         // Get actual and estimated time in int format.
         bool isParsable = TimeToInt(method, out int actualTime);
         int estimatedTime = WorkloadImitator.GetEstimatedTime(nameof(WorkloadImitator.M2)) + 100;
@@ -166,14 +137,14 @@ public class TracerTest
         Assert.InRange(actualTime, actualTime, estimatedTime);
         Assert.Equal(nameof(WorkloadImitator.M2), method.Name);
         Assert.Equal(nameof(WorkloadImitator), method.Class);
-        Assert.Equal(1, method.Methods.Count);
+        Assert.Equal(1, method.Methods!.Count);
         AssertM0(method.Methods[0]);
     }
 
     private void AssertTime(ThreadInfo thread)
     {
         int sum = 0;
-        foreach (MethodInfo info in thread.Methods)
+        foreach (var info in thread.Methods)
         {
             TimeToInt(info, out int delta);
             sum += delta;
