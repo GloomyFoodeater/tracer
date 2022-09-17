5ec533cf6fcf4326ca46804335e6171f5af594e6
Siarhei Paulavets <s.paulavets@gmail.com>
2022-09-17 17:59:18 +0300

    Add yaml serializer

---
+++ Tracer.Serialization/Tracer.Serialization.Yaml/YamlTraceResultSerializer.cs
@@ -0,0 +1,18 @@
+﻿using Tracer.Core;
+using Tracer.Serialization.Abstractions;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace Tracer.Serialization.Yaml;
+
+public class YamlTraceResultSerializer : ITraceResultSerializer
+{
+    public string Format => "yaml";
+
+    public void Serialize(TraceResult traceResult, Stream to)
+    {
+        var serializer = new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+        using var writer = new StreamWriter(to);
+        serializer.Serialize(writer, traceResult);
+    }
+}
