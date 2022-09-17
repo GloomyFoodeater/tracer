f99c5ad6f13547f3dc897146dff6ec83518cb640
Siarhei Paulavets <s.paulavets@gmail.com>
2022-09-17 17:59:19 +0300

    Fix thread sync in Tracer

--- Tracer/Tracer.Core/Tracer.cs
+++ Tracer/Tracer.Core/Tracer.cs
@@ -6,16 +6,19 @@ public class Tracer : ITracer
 {
     private readonly List<ThreadNode> _threads = new();
 
+    private readonly object _threadsLock = new object();
+
     public void StartTrace()
     {
         // Get thread node to update.
         int tid = Thread.CurrentThread.ManagedThreadId;
-        ThreadNode? threadNode = _threads.Find(node => node.Id == tid);
-
-        // Add new thread node to list.
-        if (threadNode == null)
+        ThreadNode? threadNode;
+        lock (_threadsLock)
         {
-            lock (new object())
+            threadNode = _threads.Find(node => node.Id == tid);
+
+            // Add new thread node to list.
+            if (threadNode == null)
             {
                 threadNode = new ThreadNode(tid);
                 _threads.Add(threadNode);
@@ -36,7 +39,11 @@ public class Tracer : ITracer
     {
         // Get thread node to update.
         int tid = Thread.CurrentThread.ManagedThreadId;
-        ThreadNode? threadNode = _threads.Find(node => node.Id == tid);
+        ThreadNode? threadNode;
+        lock (_threadsLock)
+        {
+            threadNode = _threads.Find(node => node.Id == tid);
+        }
 
         // Stop adding method to method tree.
         threadNode?.StopAdd();
@@ -46,9 +53,12 @@ public class Tracer : ITracer
     {
         // Covert thread nodes to infos.
         List<ThreadInfo> threadInfos = new();
-        foreach (ThreadNode threadNode in _threads)
+        lock (_threadsLock)
         {
-            threadInfos.Add(threadNode.ToThreadInfo());
+            foreach (ThreadNode threadNode in _threads)
+            {
+                threadInfos.Add(threadNode.ToThreadInfo());
+            }
         }
 
         return new(threadInfos);
