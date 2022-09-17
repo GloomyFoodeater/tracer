7d7a08b6c8cda2cde8d3e8cb7a9552560c840888
Siarhei Paulavets <s.paulavets@gmail.com>
2022-09-17 17:59:17 +0300

    Add json serializer

---
+++ Tracer.Serialization/Tracer.Serialization.Json/JsonTraceResultSerializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using Tracer.Core;
+using Tracer.Serialization.Abstractions;
+
+namespace Tracer.Serialization.Json;
+
+public class JsonTraceResultSerializer : ITraceResultSerializer
+{
+    public string Format => "json";
+
+    public void Serialize(TraceResult traceResult, Stream to)
+    {
+        JsonSerializerOptions options = new()
+        {
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+        JsonSerializer.Serialize(to, traceResult, options);
+    }
+}
