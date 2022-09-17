5fcac52c5f83cf17e99ee77cf16e7537e9f9ce06
Siarhei Paulavets <s.paulavets@gmail.com>
2022-09-17 17:59:19 +0300

    Add 1 thread many methods test

--- Tracer/Tracer.Core.Tests/TracerTest.cs
+++ Tracer/Tracer.Core.Tests/TracerTest.cs
@@ -17,8 +17,6 @@ public class TracerTest
         Assert.Equal(0, result.Threads.Count);
     }
     
-    private static bool TimeToInt(MethodInfo info, out int actualTime) => int.TryParse(info.Time[..^2], out actualTime);
-
     [Fact]
     public void OneMethod()
     {
@@ -39,15 +37,78 @@ public class TracerTest
         Assert.NotNull(result.Threads[0].Methods);
         Assert.Equal(1, result.Threads[0].Methods.Count);
         
+        AssertM0(result.Threads[0].Methods[0]);
+        
+        AssertTime(result.Threads[0]);
+    }
+    
+    [Fact]
+    public void ManyMethods()
+    {
+        // Arrange
+        ITracer tracer = new Tracer();
+        WorkloadImitator imitator = new(tracer);
+
+        // Act
+        imitator.M1();
+        imitator.M0();
+        imitator.M1();
+        TraceResult result = tracer.GetTraceResult();
+        
+        // Assert
+        Assert.NotNull(result);
+        
+        Assert.NotNull(result.Threads);
+        Assert.Equal(1, result.Threads.Count);
+        
+        Assert.NotNull(result.Threads[0].Methods);
+        Assert.Equal(3, result.Threads[0].Methods.Count);
+        
+        AssertM1(result.Threads[0].Methods[0]);
+        AssertM0(result.Threads[0].Methods[1]);
+        AssertM1(result.Threads[0].Methods[2]);
+
+        AssertTime(result.Threads[0]);
+    }
+
+    private bool TimeToInt(MethodInfo info, out int actualTime) => int.TryParse(info.Time[..^2], out actualTime);
+    
+    private void AssertM0(MethodInfo method)
+    {
         // Get actual and estimated time in int format.
-        MethodInfo info = result.Threads[0].Methods[0];
-        bool isParsable = TimeToInt(info, out int actualTime);
+        bool isParsable = TimeToInt(method, out int actualTime);
         int estimatedTime = WorkloadImitator.GetEstimatedTime(nameof(WorkloadImitator.M0)) + 100;
         
         Assert.True(isParsable);
         Assert.InRange(actualTime, actualTime, estimatedTime);
-        Assert.Equal(nameof(WorkloadImitator.M0),info.Name);
-        Assert.Equal(nameof(WorkloadImitator), info.Class);
-        Assert.Null(info.Methods);
+        Assert.Equal(nameof(WorkloadImitator.M0),method.Name);
+        Assert.Equal(nameof(WorkloadImitator), method.Class);
+        Assert.Null(method.Methods);
+    }
+
+    private void AssertM1(MethodInfo method)
+    {
+        // Get actual and estimated time in int format.
+        bool isParsable = TimeToInt(method, out int actualTime);
+        int estimatedTime = WorkloadImitator.GetEstimatedTime(nameof(WorkloadImitator.M1)) + 100;
+        
+        Assert.True(isParsable);
+        Assert.InRange(actualTime, actualTime, estimatedTime);
+        Assert.Equal(nameof(WorkloadImitator.M1),method.Name);
+        Assert.Equal(nameof(WorkloadImitator), method.Class);
+        Assert.Equal(2, method.Methods.Count);
+        AssertM0(method.Methods[0]);
+        AssertM0(method.Methods[1]);
+    }
+
+    private void AssertTime(ThreadInfo thread)
+    {
+        int sum = 0;
+        foreach (MethodInfo info in thread.Methods)
+        {
+            TimeToInt(info, out int delta);
+            sum += delta;
+        }
+        Assert.Equal(thread.Time, sum + "ms");
     }
 }
--- Tracer/Tracer.Core.Tests/WorkloadImitator.cs
+++ Tracer/Tracer.Core.Tests/WorkloadImitator.cs
@@ -16,12 +16,23 @@ internal class WorkloadImitator
         _tracer.StopTrace();
     }
 
+    public void M1()
+    {
+        _tracer.StartTrace();
+        Thread.Sleep(50);
+        M0();
+        M0();
+        _tracer.StopTrace();
+    }
+
     public static int GetEstimatedTime(string method)
     {
         switch (method)
         {
             case nameof(M0):
                 return 100;
+            case nameof(M1):
+                return 250;
             default:
                 return 0;
         }
