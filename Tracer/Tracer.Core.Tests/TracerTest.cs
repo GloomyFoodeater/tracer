2dd741c0ef823c732167db90d4db26e095aae0db
Siarhei Paulavets <s.paulavets@gmail.com>
2022-09-17 17:59:19 +0300

    Add 1 thread 1 method test

--- Tracer/Tracer.Core.Tests/TracerTest.cs
+++ Tracer/Tracer.Core.Tests/TracerTest.cs
@@ -16,4 +16,38 @@ public class TracerTest
         Assert.NotNull(result.Threads);
         Assert.Equal(0, result.Threads.Count);
     }
+    
+    private static bool TimeToInt(MethodInfo info, out int actualTime) => int.TryParse(info.Time[..^2], out actualTime);
+
+    [Fact]
+    public void OneMethod()
+    {
+        // Arrange
+        ITracer tracer = new Tracer();
+
+        // Act
+        WorkloadImitator imitator = new(tracer);
+        imitator.M0();
+        TraceResult result = tracer.GetTraceResult();
+        
+        // Assert
+        Assert.NotNull(result);
+        
+        Assert.NotNull(result.Threads);
+        Assert.Equal(1, result.Threads.Count);
+        
+        Assert.NotNull(result.Threads[0].Methods);
+        Assert.Equal(1, result.Threads[0].Methods.Count);
+        
+        // Get actual and estimated time in int format.
+        MethodInfo info = result.Threads[0].Methods[0];
+        bool isParsable = TimeToInt(info, out int actualTime);
+        int estimatedTime = WorkloadImitator.GetEstimatedTime(nameof(WorkloadImitator.M0)) + 100;
+        
+        Assert.True(isParsable);
+        Assert.InRange(actualTime, actualTime, estimatedTime);
+        Assert.Equal(nameof(WorkloadImitator.M0),info.Name);
+        Assert.Equal(nameof(WorkloadImitator), info.Class);
+        Assert.Null(info.Methods);
+    }
 }
---
+++ Tracer/Tracer.Core.Tests/WorkloadImitator.cs
@@ -0,0 +1,29 @@
+﻿namespace Tracer.Core.Tests;
+
+internal class WorkloadImitator
+{
+    private readonly ITracer _tracer;
+
+    public WorkloadImitator(ITracer tracer)
+    {
+        _tracer = tracer;
+    }
+
+    public void M0()
+    {
+        _tracer.StartTrace();
+        Thread.Sleep(100);
+        _tracer.StopTrace();
+    }
+
+    public static int GetEstimatedTime(string method)
+    {
+        switch (method)
+        {
+            case nameof(M0):
+                return 100;
+            default:
+                return 0;
+        }
+    }
+}
